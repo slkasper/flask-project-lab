bdcb55783a1cf90e11e7d03149632078a65c2af1
Sonya Kasper <slkasper@gmail.com>
2023-03-23 21:11:41 -0400

    added postgres connection and unused sql table

--- migrations/create_tables.sql
+++ migrations/create_tables.sql
@@ -1,6 +1,17 @@
-DROP TABLE IF EXISTS houses;
+DROP TABLE IF EXISTS housing;
 
-CREATE TABLE IF NOT EXISTS houses (
-    id serial PRIMARY KEY,
+-- usecols = ["Id", "TotalBsmtSF", "1stFlrSF", "2ndFlrSF", "TotRmsAbvGrd", "Neighborhood", "SalePrice"]
+-- NOT USING THIS!!! CREATING A PANDAS SOLUTION BECAUSE THE 1stFlr and 2ndFlr are causing issues in manual setup
 
+    CREATE TABLE IF NOT EXISTS housing (
+    Id serial PRIMARY KEY,
+    TotalBsmtSF INTEGER,
+    1stFlrSF INTEGER,
+    2ndFlrSF INTEGER,
+    TotRmsAbvGrd INTEGER,
+    Neighborhood VARCHAR(255) NOT NULL,
+    price INTEGER,
+    --created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX IF NOT EXISTS housing_price_index ON housing (price);
